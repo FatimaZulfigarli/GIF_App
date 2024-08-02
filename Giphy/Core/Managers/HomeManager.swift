d0b3d3f6042d14e43e8c828a3a0d362ad5037b62
FatimaZulfigarli <113666959+FatimaZulfigarli@users.noreply.github.com>
2024-08-02 21:01:06 +0400

    Error fixed
    Endpoint url problem

--- Giphy/Core/Managers/HomeManager.swift
+++ Giphy/Core/Managers/HomeManager.swift
@@ -6,36 +6,14 @@
 //
 
 import Foundation
-import Alamofire
+
 
 class HomeManager {
     func fetchTrendingGIFs(completion: @escaping (GIFModel?, String?) -> Void) {
-        NetworkManager.request(model: GIFModel.self, endpoint: .gifsTrending) { response, error in
-            if let error = error {
-                completion(nil, error)
-            } else {
-                // Log the raw response data if needed
-                if let jsonData = try? JSONEncoder().encode(response),
-                   let jsonString = String(data: jsonData, encoding: .utf8) {
-                   
-                }
-                completion(response, nil)
-            }
-        }
+        NetworkManager.request(model: GIFModel.self, endpoint: .gifsTrending, completion: completion) 
     }
     
     func fetchTrendingStickers(completion: @escaping (StickerModel?, String?) -> Void) {
-        NetworkManager.request(model: StickerModel.self, endpoint: .stickersTrending) { response, error in
-            if let error = error {
-                completion(nil, error)
-            } else {
-                // Log the raw response data if needed
-                if let jsonData = try? JSONEncoder().encode(response),
-                   let jsonString = String(data: jsonData, encoding: .utf8) {
-                   
-                }
-                completion(response, nil)
-            }
-        }
+        NetworkManager.request(model: StickerModel.self, endpoint: .stickersTrending, completion: completion)
     }
 }
